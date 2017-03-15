0577b3200947b6933f66dbf0906e3c4f9a724442
Kevin Zhang <kevin@Kevins-MacBook-Pro.local>
2017-03-15 00:18:47 -0400

    Initial Commit

---
+++ parseDatas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  parseDatas
+//
+//  Created by Kevin Zhang on 2017-03-15.
+//  Copyright © 2017 Kevin Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
